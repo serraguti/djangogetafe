b6933bfbf0718424769d2c2b459d541ce793899f
serraguti <pacoserranox@gmail.com>
2025-03-07 19:11:34 +0100

    numero 3 templates html

--- webproject/aplicacion/urls.py
+++ webproject/aplicacion/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from aplicacion import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('viernes/', views.metodoViernes, name='viernes')
 ]
 #http://127.0.0.1:8000/magia/
 #http://127.0.0.1:8000/magia/futbol/
--- webproject/aplicacion/views.py
+++ webproject/aplicacion/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Mi primera pagina Django!!!")
+
+def metodoViernes(request):
+    return HttpResponse("Hoy es viernes, mañana sábado")
