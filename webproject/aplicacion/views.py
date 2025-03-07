22fe2b6ca29e1d0436edc56d38ebe70909c95f33
serraguti <pacoserranox@gmail.com>
2025-03-07 19:42:11 +0100

    paginas html

--- webproject/aplicacion/views.py
+++ webproject/aplicacion/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Mi primera pagina Django!!!")
+    return render(request, 'aplicacion/index.html')
+    #return HttpResponse("Mi primera pagina Django!!!")
 
 def metodoViernes(request):
     return HttpResponse("Hoy es viernes, mañana sábado")
