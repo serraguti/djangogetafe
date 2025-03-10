383cb06a90b9fac0fc612469120d5e364ea9c683
serraguti <pacoserranox@gmail.com>
2025-03-10 20:22:07 +0100

    a

--- proyectoweb/informacion/urls.py
+++ proyectoweb/informacion/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from informacion import views
 
 urlpatterns=[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('pelis/', views.pelis, name='pelis')
 ]
--- proyectoweb/informacion/views.py
+++ proyectoweb/informacion/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'informacion/index.html')
+
+def pelis(request):
+    return render(request, 'informacion/pelis.html')
