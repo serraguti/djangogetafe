746acc109a6c4a007335b64c0e35ba62321af1e8
serraguti <pacoserranox@gmail.com>
2025-03-18 16:46:34 +0100

    personajes series

--- proyectochulo/television/urls.py
+++ proyectochulo/television/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('series/', views.metodoSeries, name='series'),
     path('personajes/', views.metodoPersonajes, name='personajes'),
+    path('personajesseries/', views.personajesSeries, name='personajesseries'),
 ]
--- proyectochulo/television/views.py
+++ proyectochulo/television/views.py
@@ -5,6 +5,19 @@ from television.models import ServiceSeries
 def index(request):
     return render(request, 'pages/index.html')
 
+def personajesSeries(request):
+    #PREGUNTAMOS SI HEMOS RECIBIDO EL DATO DE idserie EN GET
+    if ('idserie' in request.GET):
+        servicio = ServiceSeries()
+        idserie = request.GET['idserie']
+        personajes = servicio.getPersonajesSerie(idserie)
+        context = {
+            "personajes": personajes
+        }
+        return render(request, 'pages/personajesserie.html', context)
+    else:
+        return render(request, 'pages/personajesserie.html')
+
 def metodoPersonajes(request):
     servicio = ServiceSeries()
     personajes = servicio.getPersonajes()
