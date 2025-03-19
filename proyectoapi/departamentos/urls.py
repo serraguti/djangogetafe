c35c6091d4e96904fdc999a8d32bfaab1e3cade1
serraguti <pacoserranox@gmail.com>
2025-03-19 18:14:04 +0100

    create complete

--- proyectoapi/departamentos/urls.py
+++ proyectoapi/departamentos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from departamentos import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create/', views.insertarDepartamento, name='create')
 ]
--- proyectoapi/departamentos/views.py
+++ proyectoapi/departamentos/views.py
@@ -9,3 +9,18 @@ def index(request):
         "departamentos": departamentos
     }
     return render(request, 'pages/index.html', context)
+
+def insertarDepartamento(request):
+    if ('cajaid' in request.POST):
+        servicio = ServiceDepartamentos()
+        id = request.POST['cajaid']
+        nombre = request.POST['cajanombre']
+        localidad = request.POST['cajalocalidad']
+        servicio.insertarDepartamento(id, nombre, localidad)
+        departamentos = servicio.getDepartamentos()
+        context = {
+            "departamentos": departamentos
+        }
+        return render(request, 'pages/index.html', context)
+    else:
+        return render(request, 'pages/create.html')
